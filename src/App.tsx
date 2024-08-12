05574ac0ec801ccd3805991799ea813a2a79593b
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-08-12 13:53:38 +0500

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import Header from '../src/components/header'; // Ensure the path is correct
 
 const App: React.FC = () => {
   return (
-    <div className="bg-custom-bg bg-fixed bg-cover bg-center m-0 p-8 min-h-screen">
-      <Header />
-    </div>
+    <>
+    <Header/>
+    <main>
+    
+   </main>
+   </>
   );
 }
 
