99dcaf5d8af58aad3ed090e4f741ad3f38c99d22
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-08-12 12:27:32 +0500

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../src/components/header'; // Ensure the path is correct
+import Quiz from './components/quiz';
 
 const App: React.FC = () => {
   return (
     <div className="bg-custom-bg bg-fixed bg-cover bg-center m-0 p-8 min-h-screen">
       <Header />
+      <Quiz/>
     </div>
   );
 }
---
+++ src/components/quiz.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Quiz : React.FC = () => {
+  return (
+    <div>
+      <h1>Hello Quiz</h1>
+    </div>
+  )
+}
+export default Quiz;
