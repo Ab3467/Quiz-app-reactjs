4df47dae02f6f1a884a77a5882cbdc51b033570a
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-08-12 11:57:27 +0500

    changes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Image from "../assets/quiz-logo.png"
 
  const Header:React.FC =()=> {
   return (
     <div>
-      
+      <img src={Image} alt="logo-image" />
     </div>
   )
 }
