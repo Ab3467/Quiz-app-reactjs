1ded24d570ae1eeb3b8f264bbbfbbb0bc16c060a
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-08-12 15:37:25 +0500

    Changes

--- src/components/answers.tsx
+++ src/components/answers.tsx
@@ -16,24 +16,26 @@ const Answers: React.FC<AnswersProps> = ({ answers, AnswerState, selectedAnswers
   }
 
   return (
-    <ul id="answers">
+    <ul className="list-none m-0 p-0 flex flex-col items-center gap-2">
       {shuffledAnswers.current.map((answer) => {
         const isSelected = selectedAnswers === answer;
-        let cssClass = '';
+        let buttonClasses = 'inline-block w-full font-["Roboto Condensed"] text-base py-4 px-8 border-none rounded-full transition-all duration-200 ease-in-out disabled:opacity-50';
 
         if (AnswerState === 'answered' && isSelected) {
-          cssClass = 'selected';
-        }
-
-        if ((AnswerState === 'correct' || AnswerState === 'wrong') && isSelected) {
-          cssClass = AnswerState; // Convert to lowercase for consistency
+          buttonClasses += ' bg-[#f5a76c] text-[#2c203d]';
+        } else if (AnswerState === 'correct' && isSelected) {
+          buttonClasses += ' bg-[#5af59d] text-[#2c203d]';
+        } else if (AnswerState === 'wrong' && isSelected) {
+          buttonClasses += ' bg-[#f55a98] text-[#2c203d]';
+        } else {
+          buttonClasses += ' bg-[#6cb7f5] hover:bg-[#9d5af5] hover:text-white focus:bg-[#9d5af5] focus:text-white';
         }
 
         return (
-          <li key={answer} className="answer">
-            <button 
-              onClick={() => onSelect(answer)} 
-              className={cssClass} 
+          <li key={answer} className="w-[90%] mx-auto">
+            <button
+              onClick={() => onSelect(answer)}
+              className={buttonClasses}
               disabled={AnswerState !== ''}
             >
               {answer}
