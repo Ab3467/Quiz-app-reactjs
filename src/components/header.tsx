30dc60fe78bb5bc1410eccbb1e0b16c27f7cfea1
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-08-12 12:15:10 +0500

    Changes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,12 +3,12 @@ import Image from "../assets/quiz-logo.png"; // Ensure the path is correct
 
 const Header: React.FC = () => {
   return (
-    <div className="flex flex-col items-center p-4">
-      <img src={Image} alt="logo-image" className="mb-4" />
+    <header className="">
+      <img src={Image} alt="logo-image" className="w-12 h-12"/>
       <h1 className="font-roboto-condensed font-bold text-4xl tracking-wider uppercase bg-gradient-to-r from-gradient-start to-gradient-end bg-clip-text text-transparent">
         React Quiz
       </h1>
-    </div>
+    </header>
   );
 }
 
