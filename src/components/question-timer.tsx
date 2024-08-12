f91bca9c92b20c6bdafc45a47f4aa180a496451a
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-08-12 16:08:31 +0500

    Changes

--- src/components/question-timer.tsx
+++ src/components/question-timer.tsx
@@ -11,23 +11,24 @@ const QuestionTimer: React.FC<QuestionTimerProps> = ({ timeout, onTimeOut, mode 
   const [remainingTime, setRemainingTime] = useState<number>(timeout);
 
   useEffect(() => {
-    // Clear the previous timer and set a new one
-    const timer = setTimeout(onTimeOut, remainingTime);
-    return () => clearTimeout(timer);
-  }, [remainingTime, onTimeOut]);
-
-  useEffect(() => {
+    if (remainingTime <= 0) {
+      onTimeOut();
+      return;
+    }
+    
     // Decrement the remaining time every 100ms
     const interval = setInterval(() => {
       setRemainingTime((prevRemainingTime) => Math.max(prevRemainingTime - 100, 0));
     }, 100);
+    
+    // Clear interval and timer on component unmount or when timeout changes
     return () => clearInterval(interval);
-  }, []);
+  }, [remainingTime, onTimeOut]);
 
   return (
     <Progress
       max={timeout}
-      value={remainingTime}
+      value={timeout - remainingTime}
       className={`w-1/2 h-2 rounded-full ${mode === 'answered' ? 'bg-[#f8e59c]' : mode === 'correct' ? 'bg-[#5af59d]' : mode === 'wrong' ? 'bg-[#f55a98]' : 'bg-[#9e5ef8]'} transition-colors duration-300`}
     />
   );
