1a326990b71c262bfedb98baf81705209b44c2a6
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-08-12 15:36:52 +0500

    Changes

--- src/components/question-timer.tsx
+++ src/components/question-timer.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 type QuestionTimerProps = {
   timeout: number;
   onTimeOut: () => void;
-  mode: string;
+  mode: 'answered' | 'correct' | 'wrong' | '';
 }
 
 const QuestionTimer: React.FC<QuestionTimerProps> = ({ timeout, onTimeOut, mode }) => {
@@ -11,21 +11,24 @@ const QuestionTimer: React.FC<QuestionTimerProps> = ({ timeout, onTimeOut, mode 
 
   useEffect(() => {
     const timer = setTimeout(onTimeOut, timeout);
-    return () => {
-      clearTimeout(timer);
-    };
+    return () => clearTimeout(timer);
   }, [timeout, onTimeOut]);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
     }, 100);
-    return () => {
-      clearInterval(interval);
-    };
+    return () => clearInterval(interval);
   }, []);
 
-  return <progress id="question-time" max={timeout} value={remainingTime} className={mode} />;
-}
+  return (
+    <progress
+      id="question-time"
+      max={timeout}
+      value={remainingTime}
+      className={`w-1/2 h-2 rounded-full ${mode === 'answered' ? 'bg-[#f8e59c]' : 'bg-[#9e5ef8]'} text-[#9e5ef8]`}
+    />
+  );
+};
 
 export default QuestionTimer;
